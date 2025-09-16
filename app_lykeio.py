00f780cb90d91df1406fd63d6c17a1db7ee98d69
samarast-svg <samaras_t@yahoo.gr>
2025-09-16 21:29:40 +0300

    Update app_lykeio.py

--- app_lykeio.py
+++ app_lykeio.py
@@ -2,10 +2,12 @@ import streamlit as st
 
 st.set_page_config(page_title="Home", page_icon="🏠")
 
-st.title("🏠 Καλωσήρθες")
+st.title("🏠 Welcome to GenEdu")
+st.subheader("An interactive Genetics Education Tool for High School Biology")
 
 st.markdown("Χρησιμοποίησε το μενού αριστερά ή πάτησε παρακάτω για να ξεκινήσεις:")
 
 if st.button("📚 Μετάβαση στη Library"):
     st.switch_page("pages/1_Library.py")
+
 
