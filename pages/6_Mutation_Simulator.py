a48c80d7b678015c288d0248d9f1036b9d935417
samarast-svg <samaras_t@yahoo.gr>
2026-02-11 19:33:21 +0200

    Update 6_Mutation_Simulator.py

--- pages/6_Mutation_Simulator.py
+++ pages/6_Mutation_Simulator.py
@@ -185,7 +185,7 @@ with col2:
     st.write("mRNA:", split_codons(mutated_mrna))
     st.write("Πρωτεΐνη:", " - ".join(mutated_protein))
 
-mutation_result = classify_mutation(
+mutation_result = classify_mutation,(
     original_protein,
     mutated_protein,
     mutation_choice = st.selectbox, (
