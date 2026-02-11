b0d51701c7b8ef85626b0d57b20b1ff3b89d72d3
samarast-svg <samaras_t@yahoo.gr>
2026-02-11 18:32:07 +0200

    Update 6_Mutation_Simulator.py

--- pages/6_Mutation_Simulator.py
+++ pages/6_Mutation_Simulator.py
@@ -9,9 +9,25 @@ st.write("Παρατήρησε πώς οι μεταλλάξεις αλλάζου
 # -------------------------------------------------
 
 genetic_code = {
-    "AUG":"Met", "GAA":"Glu", "UUU":"Phe", "CGA":"Arg",
-    "AAU":"Asn", "UUC":"Phe", "GAU":"Asp",
-    "UAA":"STOP", "UAG":"STOP", "UGA":"STOP"
+    "UUU":"Phe","UUC":"Phe","UUA":"Leu","UUG":"Leu",
+    "UCU":"Ser","UCC":"Ser","UCA":"Ser","UCG":"Ser",
+    "UAU":"Tyr","UAC":"Tyr","UAA":"STOP","UAG":"STOP",
+    "UGU":"Cys","UGC":"Cys","UGA":"STOP","UGG":"Trp",
+
+    "CUU":"Leu","CUC":"Leu","CUA":"Leu","CUG":"Leu",
+    "CCU":"Pro","CCC":"Pro","CCA":"Pro","CCG":"Pro",
+    "CAU":"His","CAC":"His","CAA":"Gln","CAG":"Gln",
+    "CGU":"Arg","CGC":"Arg","CGA":"Arg","CGG":"Arg",
+
+    "AUU":"Ile","AUC":"Ile","AUA":"Ile","AUG":"Met",  # start codon
+    "ACU":"Thr","ACC":"Thr","ACA":"Thr","ACG":"Thr",
+    "AAU":"Asn","AAC":"Asn","AAA":"Lys","AAG":"Lys",
+    "AGU":"Ser","AGC":"Ser","AGA":"Arg","AGG":"Arg",
+
+    "GUU":"Val","GUC":"Val","GUA":"Val","GUG":"Val",
+    "GCU":"Ala","GCC":"Ala","GCA":"Ala","GCG":"Ala",
+    "GAU":"Asp","GAC":"Asp","GAA":"Glu","GAG":"Glu",
+    "GGU":"Gly","GGC":"Gly","GGA":"Gly","GGG":"Gly"
 }
 
 original_dna = "ATGGAATTTCGATAA"
